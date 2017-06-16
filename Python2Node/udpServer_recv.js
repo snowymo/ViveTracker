122a16a7b734f9ed7dc82631b6f21250a17edcac
snowymo <snowymohehe@gmail.com>
2017-06-16 15:43:27 -0400

    update python receiver with udp port, update customized relay to broadcast vive tracker information

--- Python2Node/udpServer_recv.js
+++ Python2Node/udpServer_recv.js
@@ -1,17 +1,60 @@
-var PORT = 10000;
-var HOST = '127.0.0.1';
+var udp = require('dgram');
+const holojam = require('holojam-node')(['relay']);
+// --------------------creating a udp server --------------------
 
-var dgram = require('dgram');
-var server = dgram.createSocket('udp4');
+// creating a udp server
+var server = udp.createSocket('udp4');
 
-server.on('listening', function () {
-    var address = server.address();
-    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+// emits when any error occurs
+server.on('error',function(error){
+  console.log('Error: ' + error);
+  server.close();
 });
 
-server.on('message', function (message, remote) {
-    console.log(remote.address + ':' + remote.port +' - ' + message);
+// emits on new datagram msg
+server.on('message',function(msg,info){
+  //console.log('Data received from client : ' + msg.toString());
+  console.log('Received %d bytes from %s:%d\n',msg.length, info.address, info.port);
+  console.log('Data received from client : ' + msg);
+  var jsonObj = JSON.parse(msg.toString());
+  var liveObj = {
+                  label: 'VT-'+jsonObj.name, 
+				  vector3s: [{x: parseFloat(jsonObj.x), y: parseFloat(jsonObj.y), z: parseFloat(jsonObj.z)}],
+				  vector4s: [{qx: parseFloat(jsonObj.qx), qy: parseFloat(jsonObj.qy), qz: parseFloat(jsonObj.qz), qw: parseFloat(jsonObj.qw)}]
+               };
+  console.log('live Obj to send' + JSON.stringify(liveObj));
+  holojam.Send(holojam.BuildUpdate('Vive', [liveObj]));
+  
+//sending msg
+// server.send(msg,info.port,'localhost',function(error){
+//   if(error){
+//     client.close();
+//   }else{
+//     console.log('Data sent !!!');
+//   }
+
+// });
 
 });
 
-server.bind(PORT, HOST);
+//emits when socket is ready and listening for datagram msgs
+server.on('listening',function(){
+  var address = server.address();
+  var port = address.port;
+  var family = address.family;
+  var ipaddr = address.address;
+  console.log('Server is listening at port ' + port);
+  console.log('Server ip :' + ipaddr);
+  console.log('Server is IP4/IP6 : ' + family);
+});
+
+//emits after the socket is closed using socket.close();
+server.on('close',function(){
+  console.log('Socket is closed !');
+});
+
+server.bind(10000);
+
+// setTimeout(function(){
+// server.close();
+// },8000);
--- Python2Node/updServer_send.py
+++ Python2Node/updServer_send.py
@@ -1,13 +1,55 @@
+import triad_openvr
+import time
+import sys
 import socket
+import json
+
+v = triad_openvr.triad_openvr()
+v.print_discovered_objects()
+
+if len(sys.argv) == 1:
+    interval = 1/float(120)
+elif len(sys.argv) == 2:
+    interval = 1/float(sys.argv[0])
+else:
+    print("Invalid number of arguments")
+    interval = False
+
+valid_devices = ["tracker_1"]
 
 UDP_IP = "127.0.0.1"
-UDP_PORT = 5005
-MESSAGE = "Hello, World!"
+UDP_PORT = 10000
+MESSAGE = b"Hello, World!"
  
-print "UDP target IP:", UDP_IP
-print "UDP target port:", UDP_PORT
-print "message:", MESSAGE
+print ("UDP target IP:", UDP_IP)
+print ("UDP target port:", UDP_PORT)
+print ("message:", MESSAGE)
  
 sock = socket.socket(socket.AF_INET, # Internet
                       socket.SOCK_DGRAM) # UDP
 sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
+
+if interval:
+    while(True):
+        start = time.time()
+        txt = ""
+        for device_key in v.devices.keys():
+            if device_key not in valid_devices:
+                continue
+            new_data = {}
+            data_ = v.devices[device_key].get_pose_quaternion()
+            new_data['name'] = device_key
+            new_data['x'] = data_[0]
+            new_data['y'] = data_[1]
+            new_data['z'] = data_[2]
+            new_data['qw'] = data_[3]
+            new_data['qx'] = data_[4]
+            new_data['qy'] = data_[5]
+            new_data['qz'] = data_[6]
+
+            jsondata = json.dumps(new_data)
+            sock.sendto(jsondata.encode('utf-8'), (UDP_IP, UDP_PORT))
+        
+        sleep_time = interval-(time.time()-start)
+        if sleep_time>0:
+            time.sleep(sleep_time)
